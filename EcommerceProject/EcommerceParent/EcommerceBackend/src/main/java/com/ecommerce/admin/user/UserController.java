f8e157a263da2333921f20ee24b813ac4490aeaf
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-03-12 19:30:32 +0300

    updateUser endpoint added and some userService funcs updated

--- EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserController.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserController.java
@@ -5,10 +5,7 @@ import com.ecommerce.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,6 +43,19 @@ public class UserController {
         System.out.println(user);
         service.save(user);
         return "User Created";
+
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public User editUser(@PathVariable(name="id") Integer id){
+        try{
+            User user = service.get(id);
+            return user;
+        }
+        catch (UserNotFoundException ex){
+
+        }
+        return null;
 
     }
 }
--- EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserService.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserService.java
@@ -3,10 +3,12 @@ package com.ecommerce.admin.user;
 import com.ecommerce.common.entity.Role;
 import com.ecommerce.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -25,7 +27,19 @@ public class UserService {
         return (List<Role>) roleRepository.findAll();
     }
     public void save(User user){
-        encodePassword(user);
+        Boolean isUpdatingUser= (user.getId() == null);
+        if(isUpdatingUser){
+        User existingUser = userRepository.findById(user.getId()).get();
+        if(user.getPassword().isEmpty()){
+            user.setPassword(existingUser.getPassword());
+        }
+        else{
+            encodePassword(user);
+        }
+        }
+        else {
+            encodePassword(user);
+        }
 
         userRepository.save(user);
     }
@@ -35,9 +49,29 @@ public class UserService {
         user.setPassword(encodedPassword);
     }
 
-    public boolean isEmailUnique(String email){
-        User user = userRepository.getUserByEmail(email);
-        return user == null;
+    public boolean isEmailUnique(Integer id,String email){
+        User userByEmail = userRepository.getUserByEmail(email);
+        if(userByEmail == null) return true;
+        boolean isCreatingNew = (id==null);
+        if(isCreatingNew) {
+            if (userByEmail != null) return false;
+        }
+            else{
+                if(userByEmail.getId() != id){
+                    return false;
+                }
+            }
+
+
+        return true;
     }
 
+    public User get(Integer id) throws UserNotFoundException {
+        try{
+            return userRepository.findById(id).get();
+        }
+        catch (NoSuchElementException ex){
+            throw new UserNotFoundException("Could not found any user with ID "+ id);
+        }
+    }
 }
