7646ad8a8f69c3823ab87142d819e4213578dc3e
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-03-14 18:58:28 +0300

    filter function added

--- EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserController.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/user/UserController.java
@@ -27,15 +27,16 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> listFirstPage() {
-        return listByPage(1,"firstName","asc");
+        return listByPage(1,"firstName","asc",null);
 
     }
     @GetMapping("/users/page/{pageNum}")
     public List<User> listByPage(@PathVariable(name = "pageNum") int pageNum,
                                  @Param("sortField") String sortField,
-                                 @Param("sortDir")String sortDir){
+                                 @Param("sortDir")String sortDir,
+                                 @Param("keyword")String keyword){
 
-        Page<User> pageUser = service.listByPage(pageNum,sortField,sortDir);
+        Page<User> pageUser = service.listByPage(pageNum,sortField,sortDir,keyword);
         List<User> listUsers = pageUser.getContent();
         return listUsers;
     }
--- EcommerceProject/EcommerceParent/EcommerceBackend/src/test/java/com/ecommerce/admin/user/UserRepositoryTests.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/test/java/com/ecommerce/admin/user/UserRepositoryTests.java
@@ -129,5 +129,16 @@ public class UserRepositoryTests {
 
         assertThat(listUsers.size()).isEqualTo(pageSize);
     }
+@Test
+    public void testSearchUser(){
+        String keyword = "huso";
+    int pageNumber = 0;
+    int pageSize = 4;
+    Pageable pageable = PageRequest.of(pageNumber,pageSize);
+    Page<User> page = userRepository.findAll(keyword,pageable);
 
+    List<User> listUsers = page.getContent();
+    listUsers.forEach(user -> System.out.println(user));
+    assertThat(listUsers.size()).isGreaterThan(0);
+}
 }
