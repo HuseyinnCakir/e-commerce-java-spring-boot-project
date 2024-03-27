03c1a0e578b721d37c7bae0bbfdcdf370a280ff2
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-03-27 16:49:00 +0300

    testCategoryRepo created and entitiy class updated

--- EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/category/CategoryRepository.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/category/CategoryRepository.java
@@ -4,9 +4,10 @@ import com.ecommerce.common.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CategoryRepository extends PagingAndSortingRepository<Category,Integer> {
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
 
  
 }
