e28b41bb24fcbc1b351f00b5260411cf2c378331
Hüseyin Çakır <hsyn_cakir199@hotmail.com>
2024-03-27 18:30:13 +0300

    list categories funcs added

--- EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/category/CategoryService.java
+++ EcommerceProject/EcommerceParent/EcommerceBackend/src/main/java/com/ecommerce/admin/category/CategoryService.java
@@ -3,7 +3,9 @@ package com.ecommerce.admin.category;
 import com.ecommerce.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CategoryService {
     @Autowired
@@ -12,4 +14,38 @@ public class CategoryService {
     public List<Category> listAll(){
         return (List<Category>) categoryRepository.findAll();
     }
+
+    public List<Category> listCategoriesUsedInForm(){
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+        Iterable<Category> categoriesInDb = categoryRepository.findAll();
+        for (Category category:categoriesInDb){
+            if(category.getParent() == null){
+                listCategoriesUsedInForm().add(new Category(category.getName()));
+                Set<Category> children = category.getChildren();
+                for (Category subCategories: children){
+                    String name = "--" + subCategories.getName();
+                    listCategoriesUsedInForm().add(new Category(name));
+                    listChildren(categoriesUsedInForm,subCategories,1);
+                }
+            }
+
+
+        }
+        return  categoriesUsedInForm;
+    }
+    private void listChildren(List<Category> categoriesUsedInForm,Category parent,int subLevel){
+    int newSubLevel = subLevel + 1;
+    Set<Category> children = parent.getChildren();
+    for (Category subCategories: children){
+        String name = "";
+        for (int i = 0;i < newSubLevel; i++){
+
+            name += "--";
+        }
+        name+=subCategories.getName();
+        listCategoriesUsedInForm().add(new Category(name));
+        listChildren(categoriesUsedInForm,subCategories,newSubLevel);
+    }
+    }
+
 }
